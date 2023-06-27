239521c13153573750512a8475362f935cd5fb8e
mefae1 <mefae1@pop-os.localdomain>
2023-06-27 15:52:19 +0800

    update

--- debug.sh
+++ debug.sh
@@ -10,23 +10,54 @@ docker-compose \
 
 nuctl create project cvat
 
+# # yolov5 - 原生
+# nuctl deploy \
+# --project-name cvat \
+# --path serverless/pytorch/ultralytics/yolov5/nuclio/ \
+# --volume `pwd`/serverless/common:/opt/nuclio/common \
+# --platform local --resource-limit nvidia.com/gpu=1
+
+# # yolov5 - TC3防護具辨識
+# nuctl deploy \
+# --project-name cvat \
+# --path serverless/pytorch/ultralytics/yolov5_TC3/nuclio/ \
+# --volume `pwd`/serverless/common:/opt/nuclio/common \
+# --platform local --resource-limit nvidia.com/gpu=1
+
+# # yolov5 - 防護具辨識v2
+# nuctl deploy \
+# --project-name cvat \
+# --path serverless/pytorch/ultralytics/yolov5_ppe/nuclio/ \
+# --volume `pwd`/serverless/common:/opt/nuclio/common \
+# --platform local --resource-limit nvidia.com/gpu=1
+
+# # Segment Anything
+# nuctl deploy \
+# --project-name cvat \
+# --path serverless/pytorch/facebookresearch/sam/nuclio/ \
+# --volume `pwd`/serverless/common:/opt/nuclio/common \
+# --platform local --resource-limit nvidia.com/gpu=1
+
+# # transt
+# nuctl deploy \
+# --project-name cvat \
+# --path serverless/pytorch/dschoerk/transt/nuclio/ \
+# --volume `pwd`/serverless/common:/opt/nuclio/common \
+# --platform local --resource-limit nvidia.com/gpu=1
+
+# siammask
 nuctl deploy \
 --project-name cvat \
---path serverless/pytorch/ultralytics/yolov5/nuclio/ \
+--path serverless/pytorch/foolwood/siammask/nuclio \
 --volume `pwd`/serverless/common:/opt/nuclio/common \
 --platform local --resource-limit nvidia.com/gpu=1
 
-nuctl deploy \
---project-name cvat \
---path serverless/pytorch/ultralytics/yolov5_TC3/nuclio/ \
---volume `pwd`/serverless/common:/opt/nuclio/common \
---platform local --resource-limit nvidia.com/gpu=1
-
-nuctl deploy \
---project-name cvat \
---path serverless/pytorch/ultralytics/yolov5_HL/nuclio/ \
---volume `pwd`/serverless/common:/opt/nuclio/common \
---platform local --resource-limit nvidia.com/gpu=1
+# # yolov7 - 原生
+# nuctl deploy \
+# --project-name cvat \
+# --path serverless/onnx/WongKinYiu/yolov7/nuclio/ \
+# --volume `pwd`/serverless/common:/opt/nuclio/common \
+# --platform local --resource-limit nvidia.com/gpu=1
 
 docker-compose \
     -f docker-compose.yml \
---
+++ git.sh
@@ -0,0 +1,19 @@
+# export HOME=/home/jovyan
+# git config --global --add safe.directory /home/jovyan/Finlab
+
+git remote set-url origin git@github.com:bensonbs/cvat
+
+# git add meme
+# git add utils
+# git add pages
+# git add temp/stock_info.json
+# git add README.md
+# git add Dockerfile
+# git add *.py
+# git add *.txt
+# git add *.yml
+# git add *.sh
+# git add *.json
+git add .
+git commit -m "fix bug"
+git push -f
